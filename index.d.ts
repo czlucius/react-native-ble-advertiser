a7bf9a4b8b7bb10c2cee8cf1762f70a3836ca369
Lucius <58442255+czlucius@users.noreply.github.com>
2023-05-09 20:58:50 +0800

    Add Bluetooth constants

--- index.d.ts
+++ index.d.ts
@@ -25,3 +25,21 @@ export function enableAdapter(): void;
 export function disableAdapter(): void;
 export function getAdapterState(): Promise<string>;
 export function isActive(): Promise<boolean>;
+
+export const ADVERTISE_MODE_BALANCED: number
+export const ADVERTISE_MODE_LOW_LATENCY: number
+export const ADVERTISE_MODE_LOW_POWER: number
+export const ADVERTISE_TX_POWER_HIGH: number
+export const ADVERTISE_TX_POWER_LOW: number
+export const ADVERTISE_TX_POWER_MEDIUM: number
+export const ADVERTISE_TX_POWER_ULTRA_LOW: number
+
+export const SCAN_MODE_BALANCED: number
+export const SCAN_MODE_LOW_LATENCY: number
+export const SCAN_MODE_LOW_POWER: number
+export const SCAN_MODE_OPPORTUNISTIC: number
+export const MATCH_MODE_AGGRESSIVE: number
+export const MATCH_MODE_STICKY: number
+export const MATCH_NUM_FEW_ADVERTISEMENT: number
+export const MATCH_NUM_MAX_ADVERTISEMENT: number
+export const MATCH_NUM_ONE_ADVERTISEMENT: number
